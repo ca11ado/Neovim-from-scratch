14ccdef054cb11bdb23b7c51c2bc48db58aaae57
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2022-03-08 16:34:30 +0300

    show relative filename

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -21,7 +21,7 @@ local diff = {
 	"diff",
 	colored = false,
 	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
+	cond = hide_in_width,
 }
 
 local mode = {
@@ -74,8 +74,24 @@ lualine.setup({
 	sections = {
 		lualine_a = { branch, diagnostics },
 		lualine_b = { mode },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- Displays file status (readonly status, modified status)
+				path = 1, -- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
+
+				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+				-- for other components. (terrible name, any suggestions?)
+				symbols = {
+					modified = "[+]", -- Text to show when the file is modified.
+					readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+					unnamed = "[No Name]", -- Text to show for unnamed buffers.
+				},
+			},
+		},
+		--lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_x = { diff, spaces, "encoding", filetype },
 		lualine_y = { location },
 		lualine_z = { progress },
